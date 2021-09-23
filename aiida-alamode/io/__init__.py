ba66215e274e590c12f2d337ebdb6672f4d23b8b
masato1122 <masato1122@hotmail.co.jp>
2021-09-23 11:25:48 +0900

    first commit

---
+++ aiida-alamode/io/__init__.py
@@ -0,0 +1,54 @@
+
+
+alm_variables={
+        'general':{
+            'hessian': None,
+            'fcsym_basis': 'lattice',
+            'kd': None,
+            'magmom': None,
+            'mode': None,
+            'nat': None,
+            'nkd': None,
+            'nmaxsave': None,
+            'noncollinear': 0,
+            'periodic': [1,1,1],
+            'prefix': None,
+            'printsym': 0,
+            'tolerance': 1e-3,
+            }
+        'interaction':{
+            'nbody':
+            'norder':
+            }
+        'optimize':{
+            'conv_tol':
+            'cv':
+            'cv_minalpha':
+            'debias_ols':
+            'dfset':
+            'dfset_cv':
+            'enet_dnorm':
+            'fc2xml':
+            'fc3xml':
+            'iconst':
+            'l1_alpha':
+            'l1_ratio':
+            'lmodel':
+            'maxiter':
+            'ndata':
+            'ndata_cv':
+            'nstart':
+            'nend':
+            'nstart_cv':
+            'nend_cv':
+            'rotaxis':
+            'skip':
+            'solution_path':
+            'sparse':
+            'sparsesolver':
+            'standardize':
+            }
+        }
+
+#str_var = [
+
