ac0e339b45875102328ce7cd06c85abae99cd069
masato1122 <masato1122@hotmail.co.jp>
2021-09-23 13:09:54 +0900

    io

--- aiida_alamode/io/default.py
+++ aiida_alamode/io/default.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+from .variables import *
 
-print(alm_variables)
+def get_alm_defaults(structure=None, poscar=None):
+    
+    default_variables = alm_variables.copy()
+
+
+
+    return default_variables
 
