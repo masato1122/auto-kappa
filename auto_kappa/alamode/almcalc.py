836497914315aa2fee015100677fcfa67af476ef
masato1122 <masato1122@hotmail.co.jp>
2022-10-02 13:16:15 +0900

    almcalc.py

--- auto_kappa/alamode/almcalc.py
+++ auto_kappa/alamode/almcalc.py
@@ -603,6 +603,7 @@ class AlamodeCalc():
                 verbosity=self.verbosity
                 )
         if almdisp is None:
+            warnings.warn(" Error: Couldn't obtain AlamodeDisplace object properly.")
             return None
         
         msg = "\n"
