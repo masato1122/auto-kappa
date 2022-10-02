c197dfd71d7b4bbbcebc9f01a5daac28aa77f820
masato1122 <masato1122@hotmail.co.jp>
2022-10-02 14:23:27 +0900

    examples

--- examples/vasp_ase/run.sh
+++ examples/vasp_ase/run.sh
@@ -1,3 +1,9 @@
+#
+# This example generates VASP scripts with auto_kappa. If INCAR, POSCAR, POTCAR, and KPOINTS
+# are generated in ./out directory, they are working properly. 
+# auto_kappa must be added to PYTHONPATH before running this example.
+#
+#
 tdir=./tools
 
 python $tdir/poscar2relax.py \
