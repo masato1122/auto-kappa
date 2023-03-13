5d9d1d1326e962698d0764a00da381d0ead45649
masato1122 <masato1122@hotmail.co.jp>
2023-03-13 17:36:09 +0900

    modify the cell checker

---
+++ examples/cell_checker/do_check.sh
@@ -0,0 +1,36 @@
+
+####################################################
+##
+## Modify this part depending on your environment
+##
+dir_autokappa=/home/ohnishi/work/PhDB/auto-kappa
+dir0=~/phonondb-20180417
+####################################################
+
+tdir=$dir_autokappa/tools
+
+##
+## modify the range of ID
+##
+for id in `seq 0 10000`; do
+    
+    ##
+    ## You may also need to modify this part.
+    ##
+    dir_each=$dir0/mp-${id}
+    dir_apdb=./mp-${id}
+
+    if [ ! -e $dir_each ]; then
+        continue
+    fi
+
+    if [ ! -e $dir_apdb ]; then
+        continue
+    fi
+    
+    echo " === mp-${id} ==="
+    python $tdir/check_cellsize.py \
+        --dir_phdb $dir_each \
+        --dir_apdb $dir_apdb
+done
+
