211b5a9069e008e49686256364bc4d10433c960d
masato1122 <masato.ohnishi.ac@gmail.com>
2024-03-05 12:56:31 +0900

    fix a bug for SPGLIB

--- auto_kappa/structure/crystal.py
+++ auto_kappa/structure/crystal.py
@@ -126,14 +126,18 @@ def get_standardized_structure_spglib(
     """
     structure = change_structure_format(struct_orig, format='phonopy')
     
-    ### PhonopyAtom => tuple
-    lattice = np.array(structure.get_cell().T, dtype="double", order="C")
-    positions = np.array(structure.get_scaled_positions(), dtype="double", order="C")
-    numbers = np.array(structure.get_atomic_numbers(), dtype="intc")
-    cell = (lattice, positions, numbers)
-    
-    ### Get the standardized structure
-    out = spglib.standardize_cell(cell, to_primitive=to_primitive)
+    ### Both should provide the exactly same result.
+    try:
+        ### for new verison of spglib
+        out = spglib.standardize_cell(structure, to_primitive=to_primitive)
+    except Exception:
+        ### PhonopyAtom => tuple
+        cell = (
+                structure.get_cell(),
+                structure.get_scaled_positions(),
+                structure.get_atomic_numbers()
+                )
+        out = spglib.standardize_cell(cell, to_primitive=to_primitive)
     
     ### make the structure with the given format
     atoms = ase.Atoms(
