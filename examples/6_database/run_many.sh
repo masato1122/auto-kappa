a72577dafb89bfeb0e3a059f1f78a45ffb6bf499
masato1122 <masato.ohnishi.ac@gmail.com>
2025-07-07 16:36:31 +0900

    modify option names

--- examples/6_database/run_many.sh
+++ examples/6_database/run_many.sh
@@ -8,24 +8,19 @@ fi
 
 #############################################
 ## parameters depending on the environment
-dir_phonondb=$HOME/phonondb-20180417
+dir_phonondb=/home/apdb/phonondb-20180417
 nmax=10          ## maximum number of the loop
-ncores=32        ## number of cores to be used for VASP and Alamode
+nprocs=32        ## number of cores to be used for VASP and Alamode
 mpirun="mpirun"
 #
-#DIR_AUTOKAPPA=$HOME/src/auto-kappa
-DIR_AUTOKAPPA=/home/ohnishi/work/PhDB/auto-kappa
+DIR_AUTOKAPPA=$HOME/src/auto-kappa
 ##############################################
 
 ###########
-imin=145       ## Initial material ID 
-imax=155       ## Last material ID
+imin=140       ## Initial material ID 
+imax=160       ## Last material ID
 neach=5        ## number of materials in a loop (average : 2-3 node*days/mater)
 ###########
-
-##############################
-dir_pre_base=../6_database
-##############################
 
 for i in `seq 0 $nmax`; do
 
@@ -40,14 +35,12 @@ for i in `seq 0 $nmax`; do
         if [ ! -e $dir_db ]; then
             continue
         fi
-        
-        ### check the previous calculation
-        dir_pre=$dir_pre_base/$mpid
-        flag=`python $DIR_AUTOKAPPA/tools/check_apdb_log.py -d $dir_pre`
-        if [ $flag != "Finished_harmonic" ]; then
+
+        flag=`python $DIR_AUTOKAPPA/tools/check_apdb_log.py -d $mpid`
+        if [ $flag != "NotYet" ]; then
             continue
         fi
-        echo $mpid $flag
+        echo $mpid
         
         count=`expr $count + 1`
         if [ $count == "$neach" -o $ii == "$imax" ]; then
@@ -68,7 +61,7 @@ ofile=a.sh
 cat >$ofile <<EOF
 #!/bin/sh
 #PBS -q default
-#PBS -l nodes=1:ppn=${ncores}
+#PBS -l nodes=1:ppn=${nprocs}
 #PBS -j oe
 #PBS -N $label
 
@@ -78,35 +71,22 @@ cd \$PBS_O_WORKDIR
 
 rm ${label}.o*
 
-dir_phonondb=$dir_phonondb
-DIR_AUTOKAPPA=$DIR_AUTOKAPPA
-dir_pre_base=$dir_pre_base
-
 for id in \`seq $i0 $i1\`; do
 
     mpid="mp-\${id}"
     
-    ### check Phonondb
-    dir_db=\$dir_phonondb/\${mpid}
+    dir_db=$dir_phonondb/\${mpid}
     if [ ! -e \$dir_db ]; then
         continue
     fi
-    
-    ### check the previous calculation
-    dir_pre=\$dir_pre_base/\${mpid}
-    flag=\`python \$DIR_AUTOKAPPA/tools/check_apdb_log.py -d \$dir_pre\`
-    if [ \$flag != "Finished_harmonic" ]; then
-        continue
-    fi
-    
+
     ### start a calculation
     akrun \\
         --directory \$dir_db \\
-        --material_name \$mpid \\
-        --ncores $ncores \\
+        --outdir \$mpid \\
+        --nprocs $nprocs \\
         --mpirun ${mpirun} \\
         --anphon_para $anphon_para \\
-        --volume_relaxation 1 \\
         > $logdir/log_\${mpid}.txt
     
 done
@@ -115,7 +95,7 @@ EOF
 ################################################################
 #### Remove the comment-out before "qsub" to submit the job 
 #### and, if neccesarry, modify "qsub" command.
-#qsub $ofile
+qsub $ofile
 ################################################################
 
     if [ $i1 == $imax ]; then
