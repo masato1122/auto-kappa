e0109b22ac443f738c150a1384246853a54de981
wangzeyu <wzy979005938@gmail.com>
2025-09-18 14:59:13 +0900

    environment config

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 
 python setup.py sdist
-
+# Install auto-kappa and its dependencies in a virtual environment
+# Install matbench-discovery
+git clone https://github.com/janosh/matbench-discovery --depth 1
+uv pip install -e ./matbench-discovery
+# Install MLIPs
+# fairchem-core-1.10.0
+uv pip install -e packages/fairchem-core
+# Install auto-kappa
 uv pip install dist/auto_kappa-1.1.0.tar.gz
 
 rm -r dist
